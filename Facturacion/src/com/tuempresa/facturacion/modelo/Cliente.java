86c9e5b0d04e345b80b53abbb5bae3c79f9d380f
LicQuiterio <114039578+LicQuiterio@users.noreply.github.com>
2022-10-06 21:04:06 -0500

    leccion 4

--- Facturacion/src/com/tuempresa/facturacion/modelo/Cliente.java
+++ Facturacion/src/com/tuempresa/facturacion/modelo/Cliente.java
@@ -4,15 +4,15 @@ import javax.persistence.*;
 import org.openxava.annotations.*;
 
 import lombok.*;
-@Entity // Esto marca la clase Cliente como una entidad
-@Getter @Setter // Esto hace los campos a continuación públicamente accesible
+@Entity @Getter @Setter 
+@View(name = "Simple", members = "numero, nombre")
 public class Cliente {
-	@Id // La propiedad numero es la clave. Las claves son obligatorias (required) por
+	@Id // 
 	
-	 @Column(length=6) // La longitud de columna se usa a nivel UI y a nivel DB
+	 @Column(length=6) 
 	 int numero;
-	 @Column(length=50) // La longitud de columna se usa a nivel UI y a nivel DB
-	 @Required // Se mostrará un error de validación si la propiedad nombre se deja en blanco
+	 @Column(length=50) 
+	 @Required 
 	 String nombre;
 	 
 	 @Embedded
