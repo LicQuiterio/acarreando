c340a3d1393a275f520a91ae86e4fa7ec242a143
LicQuiterio <114039578+LicQuiterio@users.noreply.github.com>
2022-10-16 14:56:52 -0500

    lección 9
    -librerias LOMBOK
    para establecer getters y setters
    - Modificar Getter y Setter para una logica personalizada

---
+++ Facturacion/src/com/tuempresa/facturacion/modelo/Incidencia.java
@@ -0,0 +1,16 @@
+package com.tuempresa.facturacion.modelo;
+
+import lombok.*;
+
+@Getter @Setter
+public class Incidencia {
+
+	int numero;
+	String descripcion;
+	
+	public String getDescripcion() {
+		if(descripcion == null) return "Todavia no hay descripcion";
+		return descripcion;
+	}
+	
+}
