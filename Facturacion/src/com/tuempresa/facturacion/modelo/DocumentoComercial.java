f1bdb6c3e2c9c9decf4ed00cd7a6dc7a0b59ea8c
LicQuiterio <114039578+LicQuiterio@users.noreply.github.com>
2022-10-18 16:01:33 -0500

    lección 12
    Definir propiedades persistentes con @Calculation
    
    @Digits para definir tipo y longitud de los campos

--- Facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
+++ Facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
@@ -1,9 +1,11 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.math.*;
 import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
@@ -39,11 +41,29 @@ abstract public class DocumentoComercial extends Identificable{
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@ReferenceView("simple")
 	Cliente cliente;
-	  
-	@ElementCollection
-	@ListProperties("producto.numero, producto.descripcion, cantidad, precioPorUnidad, importe")
-	Collection<Detalle>detalles;
 	
 	@Stereotype("MEMO")
-	String observaciones;
+	String observaciones;  
+	
+	@ElementCollection
+	@ListProperties("producto.numero, producto.descripcion, cantidad, precioPorUnidad, " + 
+	                "importe+[" +
+			        "documentoComercial.porcentajeIVA," + 
+	                "documentoComercial.iva," +
+			        "documentoComercial.importeTotal" + 
+	                "]")
+	private Collection<Detalle> detalles;
+	
+	@Digits(integer=2, fraction=0)
+	BigDecimal porcentajeIVA;
+	
+	@ReadOnly
+	@Money
+	@Calculation("sum(detalles.importe) * porcentajeIVA / 100")
+	BigDecimal iva;
+	
+	@ReadOnly
+	@Money
+	@Calculation("sum(detalles.importe) + iva")
+	BigDecimal importeTotal;
 }
