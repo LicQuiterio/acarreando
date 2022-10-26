c05f90d3397fbb5abd99e031d4917958638db4a0
LicQuiterio <114039578+LicQuiterio@users.noreply.github.com>
2022-10-26 15:10:27 -0500

    lección 21
    Anotación Bean validation propia

--- Facturacion/src/com/tuempresa/facturacion/modelo/Producto.java
+++ Facturacion/src/com/tuempresa/facturacion/modelo/Producto.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 
 import org.openxava.annotations.*;
 
+import com.tuempresa.facturacion.anotaciones.*;
+
 import lombok.*;
 
 @Entity @Getter @Setter
@@ -34,4 +36,11 @@ public class Producto {
     @ManyToOne(fetch = FetchType.LAZY)
     @DescriptionsList
     Autor autor;
+    
+    //@Column(length=13)
+    //String isbn;
+    
+    
+    @Column(length=13) @ISBN
+    String isbn;
 }
